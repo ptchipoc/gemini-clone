0b854407173056d0edc1d519c794e24bf69d3ac2
ptchipoc <pedrotchipoco@gmail.com>
2025-11-23 10:41:57 +0100

    the project is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <Sidebar />
+      <Main />
+    </>
   )
 }
 
